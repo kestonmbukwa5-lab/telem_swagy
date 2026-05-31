741b88d5d7a5d4e6e02108f5f15b0410a0556e49
Telem Swagy <deploy@telemswagy.local>
2026-05-31 15:25:31 +0200

    Make newsletter API URL configurable via window.API_BASE

--- script.js
+++ script.js
@@ -70,7 +70,8 @@ if (newsletterForm) {
     submitBtn.textContent = 'Subscribing...';
 
     try {
-      const res = await fetch('/subscribe', {
+      const API_BASE = (window.API_BASE || '').replace(/\/$/, '');
+      const res = await fetch(`${API_BASE}/subscribe`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email }),
